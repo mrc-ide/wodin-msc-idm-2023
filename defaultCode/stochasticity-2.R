bf0e65d70b96abe832608a50b9c79f50bfb67872
juliabh <30322584+juliabh@users.noreply.github.com>
2023-11-17 10:48:46 +0000

    Update stochasticity-2.R - replaced with short course 2023
    Pete's commit on short course says "Harmonise variable names, clarify definitions and fix typos"

--- defaultCode/stochasticity-2.R
+++ defaultCode/stochasticity-2.R
@@ -1,13 +1,15 @@
-r <- user(0.1)    # Growth rate
-s <- user(0.06)   # Death rate
+r <- user(0.1)    # Birth rate
+mu <- user(0.06)   # Mortality rate
 N_init <- user(1) # Initial population size
 
+# The step size, in units of time
 dt <- 0.01
+# The current time is the number of steps taken multiplied by the step size
 time <- step * dt
 
-# Deterministic solution - often unobtainable for non-linear models
+# Deterministic solution (often unobtainable for non-linear models)
 initial(N_det) <- N_init
-update(N_det) <- N_init * exp((r - s) * time)
+update(N_det) <- N_init * exp((r - mu) * time)
 
 # Stochastic solution with initial condition, number of births in time
 # t to t + dt and then update to next step
@@ -15,10 +17,10 @@ update(N_det) <- N_init * exp((r - s) * time)
 initial(N) <- N_init
 
 # How many events happen in total (births and deaths)?
-growthAndDeath <- rbinom(N, (r + s) * dt)
+growthAndDeath <- rbinom(N, (r + mu) * dt)
 
 # How many of these events were deaths?
-death <- rbinom(growthAndDeath, s / (r + s))
+death <- rbinom(growthAndDeath, mu / (r + mu))
 growth <- growthAndDeath - death
 
 update(N) <- N + growth - death
