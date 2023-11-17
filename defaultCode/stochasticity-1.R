0bbc5b0ee4024d24be23bb0e25ee401bb4db9116
juliabh <30322584+juliabh@users.noreply.github.com>
2023-11-17 10:47:42 +0000

    Update stochasticity-1.R - replaced with short course 2023
    Pete's commit from short course repo says "Harmonise variable names, clarify definitions and fix typos"

--- defaultCode/stochasticity-1.R
+++ defaultCode/stochasticity-1.R
@@ -1,7 +1,9 @@
-r <- user(0.1)    # Growth rate
+r <- user(0.1)    # Birth rate
 N_init <- user(1) # Initial population size
 
+# The step size, in units of time
 dt <- 0.01
+# The current time is the number of steps taken multiplied by the step size
 time <- step * dt
 
 # Deterministic solution
@@ -11,6 +13,8 @@ update(N_det) <- N_init * exp(r * time)
 # Stochastic solution
 initial(N_stoch) <- N_init
 
-n_birth <- rbinom(N_stoch, r * dt)
+# We can use rbinom to generate N_stoch random samples from the 
+# binomial distribution with probability r * dt
+n_birth <- rbinom(N_stoch, r * dt) 
 
 update(N_stoch) <- N_stoch + n_birth
