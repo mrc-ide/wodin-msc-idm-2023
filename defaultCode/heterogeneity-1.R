6a56b26a52b389d2c6c5bb3ce706bdb52a4c0146
juliabh <30322584+juliabh@users.noreply.github.com>
2023-11-24 12:35:40 +0000

    Update heterogeneity-1.R - RS Nov2023
    Changes sent by Romain Nov 2023

--- defaultCode/heterogeneity-1.R
+++ defaultCode/heterogeneity-1.R
@@ -20,7 +20,7 @@ initial(I_m) <- 10
 
 N_f <- S_f+I_f
 N_m <- S_m+I_m
-N_tot <- N_f+N_m
+N_all <- N_f+N_m
 
 
 
@@ -29,7 +29,7 @@ N_tot <- N_f+N_m
 pcr_1c <-user(2)		#partner change rate
 betaMW_1c <- user(0.5)  	#per partner STI transmission probability, male to female
 betaWM_1c <- user(0.5)  	#per partner STI transmission probability, female to male
-recovery_1c <- user(2)          #recovery rate of infection per person per year (1/mean duration in years)
+recovery_1c <- user(2)          #recovery rate of infection per person per year (1/mean duration in years)		
 
 
 ##Additional outputs
@@ -37,7 +37,7 @@ recovery_1c <- user(2)          #recovery rate of infection per person per year 
 output(PopSize) <- N_f+N_m
 output(prevalence_f) <- I_f/N_f
 output(prevalence_m) <- I_m/N_m
-output(prevalence_tot) <- (I_f+I_m)/N_tot
+output(prevalence_all) <- (I_f+I_m)/N_all
 output(newInf_f) <- betaMW_1c*pcr_1c*S_f*I_m/N_m
 output(newInf_m) <- betaWM_1c*pcr_1c*S_m*I_f/N_f
-output(incidence_tot) <- betaMW_1c*pcr_1c*S_f*I_m/N_m + betaWM_1c*pcr_1c*S_m*I_f/N_f
+output(newInf_all) <- betaMW_1c*pcr_1c*S_f*I_m/N_m + betaWM_1c*pcr_1c*S_m*I_f/N_f
